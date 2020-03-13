8f36ed467abcff1876806236858a80a913938d45
andkoc001 <and.koc001@gmail.com>
2020-03-13 22:47:30 +0100

    Alphabet added

--- Pasja Informatyki/kurs_js_03/Hangman_game.js
+++ Pasja Informatyki/kurs_js_03/Hangman_game.js
@@ -21,16 +21,52 @@ function showRiddle() {
 
 window.onload = beginning; // no brackets after the function name, because it is not the function call, just alias to the function
 
+var letterArray = new Array(26);
+
+letterArray[0] = "A";
+letterArray[1] = "B";
+letterArray[2] = "C";
+letterArray[3] = "D";
+letterArray[4] = "E";
+letterArray[5] = "F";
+letterArray[6] = "G";
+letterArray[7] = "H";
+letterArray[8] = "I";
+letterArray[9] = "J";
+letterArray[10] = "K";
+letterArray[11] = "L";
+letterArray[12] = "M";
+letterArray[13] = "N";
+letterArray[14] = "O";
+letterArray[15] = "P";
+letterArray[16] = "Q";
+letterArray[17] = "R";
+letterArray[18] = "S";
+letterArray[19] = "T";
+letterArray[20] = "U";
+letterArray[21] = "V";
+letterArray[22] = "W";
+letterArray[23] = "X";
+letterArray[24] = "Y";
+letterArray[25] = "Z";
+
+
 function beginning() {
 
   var divContent = "";
 
-  for (i = 0; i <= 26; i++) {
-    divContent = divContent + '<div class = "letter">A</div>';
+
+  for (i = 0; i <= 25; i++) {
+    var element = "lett" + i;
+    divContent = divContent + '<div class = "letter" onclick="check(' + i + ')" id="' + element + '">' + letterArray[i] + '</div>';
     if ((i + 1) % 7 == 0) divContent = divContent + '<div style="clear:both;"></div>';
   }
 
   document.getElementById("alphabet").innerHTML = divContent;
 
   showRiddle();
+}
+
+function check(num) {
+  alert(num);
 }
