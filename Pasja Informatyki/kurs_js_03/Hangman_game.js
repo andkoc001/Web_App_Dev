289194470455e72e4d0fb10d414a1657076eba0a
andkoc001 <and.koc001@gmail.com>
2020-03-13 20:06:50 +0100

    Separate .css file added

--- Pasja Informatyki/kurs_js_03/Hangman_game.js
+++ Pasja Informatyki/kurs_js_03/Hangman_game.js
@@ -1,20 +1,4 @@
 /*
-Author: Andrzej Kocielski, G00376291@gmit.ie
-Description: Web Application Development, GMIT 2020, Week 7 Excercises - using the DOM
-Title: Exercise 6 - Styling with DOM
+Author: Andrzej Kocielski
+Description: Hangman Game - based on Pasja Informatyki tutorial: https://youtu.be/9FVtiJHFCSU
 */
-
-var shortcut = document.getElementById("text")
-
-
-function changeColour() {
-  shortcut.style.color = "green";
-}
-
-function changeSize() {
-  shortcut.style.fontSize = "xx-large";
-}
-
-function changeStyle() {
-  shortcut.style.fontStyle = "italic";
-}
