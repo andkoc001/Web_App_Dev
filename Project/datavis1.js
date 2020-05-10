61e04b377f1c376469c0f7e137f40e9944e76057
andkoc001 <and.koc001@gmail.com>
2020-05-10 22:10:33 +0200

    Data vis added; minor updates and tweaks

--- Project/datavis1.js
+++ Project/datavis1.js
@@ -6,198 +6,97 @@ Title: Exercise 12 - d3
 // ////////////////////
 
 
-// importing csv file with data into d3
-// d3.csv("file_name.csv")
-d3.csv("sales_data.csv")
-  // check if it was loaded - in browser's console (localhost:8000)
-  .then(function (data) {
-    console.log(data);
+// Getting data in form of an array
 
-    // convert ot numbers
-    data.forEach(function (d) {
-      d.Sales = Number(d.Sales); // or d.Sales = +d.Sales;
-    })
+// The new data variable.
+// source: https://stackoverflow.com/a/21668952
+var alphabet = [
+  { letter: "A", frequency: .08167 },
+  { letter: "B", frequency: .01492 },
+  { letter: "C", frequency: .02780 },
+  { letter: "D", frequency: .04253 },
+  { letter: "E", frequency: .12702 },
+  { letter: "F", frequency: .02288 },
+  { letter: "G", frequency: .02022 },
+  { letter: "H", frequency: .06094 },
+  { letter: "I", frequency: .06973 },
+  { letter: "J", frequency: .00153 },
+  { letter: "K", frequency: .00747 },
+  { letter: "L", frequency: .04025 },
+  { letter: "M", frequency: .02517 },
+  { letter: "N", frequency: .06749 },
+  { letter: "O", frequency: .07507 },
+  { letter: "P", frequency: .01929 },
+  { letter: "Q", frequency: .00098 },
+  { letter: "R", frequency: .05987 },
+  { letter: "S", frequency: .06333 },
+  { letter: "T", frequency: .09056 },
+  { letter: "U", frequency: .02758 },
+  { letter: "V", frequency: .01037 },
+  { letter: "W", frequency: .02465 },
+  { letter: "X", frequency: .00150 },
+  { letter: "Y", frequency: .01971 },
+  { letter: "Z", frequency: .00074 }
+];
 
+// plantes distance, diameter, density, gravity, moons relative to Earth (Earth = 1)
+// data from: https://nssdc.gsfc.nasa.gov/planetary/factsheet/planet_table_ratio.html
+var planets = [
+  { planet: "Mercury", distance: "0.387", diameter: "0.383", density: ".984", gravity: "0.", moons: "0" },
+  { planet: "Venus", distance: "0.723", diameter: "0.949", density: "0.951", gravity: "0.907", moons: "0" },
+  { planet: "Earth", distance: "1", diameter: "1", density: "1", gravity: "1", moons: "1" },
+  { planet: "Mars", distance: "1.52", diameter: "0.532", density: "0.713", gravity: "0.377", moons: "2" },
+  { planet: "Jupyter", distance: "5.2", diameter: "11.21", density: "0.24", gravity: "2.36", moons: "79" },
+  { planet: "Saturn", distance: "9.58", diameter: "9.45", density: "0.125", gravity: "0.916", moons: "82" },
+  { planet: "Uranus", distance: "19.2", diameter: "4.01", density: "0.23", gravity: "0.889", moons: "27" },
+  { planet: "Neptune", distance: "30.05", diameter: "3.88", density: ".297", gravity: "1.12", moons: "14" }
+];
 
-    // Variables for chart bar values
-    var myHeight = 350;
-    var barWidth = 40;
-    var dataCount = data.length;
-    var gap = 8;
-    var leftOffset = 50 + 0.5 * gap;
-    var myWidth = (barWidth + gap) * dataCount;
-
-    // declaree a scale for X axis
-    // mapping categorical values from the csv file
-    var xScale = d3.scaleBand()
-      .domain(data.map(function (d) {
-        return d.Month;
-      }))
-      .range([0, (myWidth + gap * (dataCount))]) // from 0 to (width of bar + gap)
-
-
-    // declare a scale for Y axis
-    var yScale = d3.scaleLinear()
-      // source of data - what is written on the axis
-      .domain([0, d3.max(data, function (d) {
-        return d.Sales;
-      }) + 0]) //.domain([0, 500])  // <-- a simple case
-      // mapped destination - where the axis is placed
-      .range([myHeight, 0]) // note the reveresed order for axes values going from bottom up
-      ; // <-- note semicolon here!
-
-
-    // create X axis
-    var xAxis = d3.axisBottom()
-      .scale(xScale);
-
-    // create Y axis
-    var yAxis = d3.axisLeft()
-      .scale(yScale); // <-- semicolon!
-
-
-    // create a svg container (still inside the curly brackets!)
-    var svgContainer = d3.select("#dataViz1").append("svg")
-      .attr("height", myHeight + 75)
-      .attr("width", myWidth * dataCount)
-      .attr("transform", "translate(0, 10)"); // offset
+var planetsDia = [.383, .949, 1, .532, 11.21, 9.45, 4.01, 3.88]; // plantes diameter relative to Earth
 
 
-    // RECTANGLE BARS
+// create svg container (canvas)
+var svgContainer = d3.select("#dataVis1").append("svg")
+  .attr("width", 400)
+  .attr("height", 2000);
 
-    // generate rectangles for Bar Chart
-    // associate variable from imported file - .data(data) - with all rectangles,
-    var myRectangle = svgContainer.selectAll('rect').data(data);
-
-
-    // enter loops - create rectangle for each instance from the data
-    myRectangle.enter().append("rect")
-
-      // transition state zero (initial)
-      .attr("fill", "#333")
-      // function will run and returns i (the index value of the array)
-      // d = data, i = index
-      .attr("x", function (d, i) {
-        return leftOffset + (i * (myWidth / dataCount + gap)); // initial offset (left margin) + width of rectangle (40) + 2 for gap between them
-      })
-      .attr("y", myHeight)
-      .attr("width", (myWidth / dataCount))
-
-      // transition state one 
-      .transition()
-      .duration(1000)
-      //.attr("y", 50) // this would produce bars hanging down, from baseline 50px from top
-      .attr("y", function (d, i) {
-        return yScale(d.Sales) // baseline 300px from top, minus (i.e. going up) the height of the rect
-      })
-      // without scaling      //.attr("height", function (d) {return d.Sales;}) // takes value of Sales from the imported csv; *10 to magnify the result
-      // with scaling
-      .attr("height", function (d) {
-        return myHeight - yScale(d.Sales);
-      })
-      .attr("fill", "steelblue")
+// draw Sun 
+var myRectangle = svgContainer.append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 400)
+  .attr("height", 40)
+  .attr("fill", "yellow");
 
 
-    // LABELS 1 - sales
+// plantes diameter
+var circles = svgContainer.selectAll("circle")
+  .data(planets);
 
-    // add label text for Bar Chart, using values from imported file - .data(data)
-    var mylabel = svgContainer.selectAll('text').data(data);
+circles.enter().append("circle")
+  .attr("cx", 200)
+  .attr("cy", function (d, i) { return 150 + (i * 250); })
+  .attr("r", function (d) {
+    return d.diameter * 10;
+  })
+  .attr("fill", function (d, i) { return "#" + (1 + i) + "" + (1 + i) + "f" }); // each iteration changes the red and green element of the #RGB parameter
 
-    // enter loops through
-    mylabel.enter().append("text")
+// planet labels
+var circles = svgContainer.selectAll("text")
+  .data(planets);
 
-      // transition state zero (initial)
-      .attr("text-anchor", "middle")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "18px")
-      .attr("fill", "steelblue")
-      .attr("x", function (d, i) {
-        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // width of rectangle (40) + 2 for gap between them
-      })
-      .attr("y", 350)
+circles.enter().append("text")
+  .attr("x", 180)
+  .attr("y", 30)
+  .text("Sun");
 
-      // transition state one 
-      .transition()
-      .duration(2000)
-      .delay(function (d, i) {
-        return 650 + (i * 25);
-      })
-      .ease(d3.easeCubicInOut) // d3.easeCircleIn, d3.easeElasticOut, d3.easeLinear
-      // positioning
-      .attr("y", function (d) {
-        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 20) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
-      })// max = 580, scale = 350
-      // text style
-      .attr("fill", "white")
-      // text content
-      .text(function (d) {
-        return d.Sales; // Sales from csv file + unicode degree symbol 
-      });
+circles.enter().append("text")
+  .attr("x", 20)
+  .attr("y", function (d, i) { return 155 + (i * 250); })
+  .text(function (d, i) { return d.planet });
 
 
-    // LABELS 2 - months
-
-    // add label text for Bar Chart, using values from imported file - .data(data)
-    //var mylabel = svgContainer.selectAll('text').data(data);
-
-    // enter loops through
-    mylabel.enter().append("text")
-
-      // transition state zero (initial)
-      .transition()
-      .duration(0)
-      .delay(2500)
-      // positioning
-      .attr("x", function (d, i) {
-        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // (50+20) to centre text in the middle of the bar; width of rectangle (40) + 2 for gap between them
-      })
-      .attr("y", 0.9 * myHeight)
-      // text style
-      .attr("text-anchor", "middle")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", "8px")
-      .attr("fill", "steelblue")
-      // text content
-      .text(function (d) {
-        return d.Month; // months names from csv file
-      })
-
-      // transition state two
-      .transition()
-      .duration(2500)
-      .delay(function (d, i) {
-        return 200 + (i * 250);
-      })
-      .ease(d3.easeBounceOut)
-      // Vertical positioning
-      .attr("y", function (d) {
-        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 30) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
-      })
-      .attr("fill", "#ccc")
-
-      // transition state three
-      .transition()
-      .duration(4000)
-      .delay(1000)
-      .attr("fill", "steelblue")
-      .ease(d3.easeLinear);
-
-
-    // AXES rendering
-
-    svgContainer.append("g") // 'g' denotes group
-      .attr("transform", "translate(50," + myHeight + ")") // note that height is inside quote marks - it is a text that need to be concatinated!
-      .call(xAxis)
-      // apply the below to all text elements
-      .selectAll("text")
-      .attr("transform", "rotate(-60)")
-      .attr("text-anchor", "end")
-      .attr("x", "-8") // by trial and error
-      .attr("y", "8");
-
-    svgContainer.append("g") // 'g' denotes group
-      .attr("transform", "translate(" + (leftOffset - 0.5 * gap) + ", 0)")
-      .call(yAxis);
-
-
-  });
+circles.enter().append("text")
+  .attr("x", 320)
+  .attr("y", function (d, i) { return 155 + (i * 250); })
+  .text(function (d, i) { return "R = " + d.diameter });
--- Project/datavis2.js
+++ Project/datavis2.js
@@ -6,47 +6,16 @@ Title: Exercise 12 - d3
 // ////////////////////
 
 
-// Getting data in form of an array, assigned to variable 'data';
-// source: https://stackoverflow.com/a/21668952
-var data = [
-  { letter: "A", frequency: .08167 },
-  { letter: "B", frequency: .01492 },
-  { letter: "C", frequency: .02780 },
-  { letter: "D", frequency: .04253 },
-  { letter: "E", frequency: .12702 },
-  { letter: "F", frequency: .02288 },
-  { letter: "G", frequency: .02022 },
-  { letter: "H", frequency: .06094 },
-  { letter: "I", frequency: .06973 },
-  { letter: "J", frequency: .00153 },
-  { letter: "K", frequency: .00747 },
-  { letter: "L", frequency: .04025 },
-  { letter: "M", frequency: .02517 },
-  { letter: "N", frequency: .06749 },
-  { letter: "O", frequency: .07507 },
-  { letter: "P", frequency: .01929 },
-  { letter: "Q", frequency: .00098 },
-  { letter: "R", frequency: .05987 },
-  { letter: "S", frequency: .06333 },
-  { letter: "T", frequency: .09056 },
-  { letter: "U", frequency: .02758 },
-  { letter: "V", frequency: .01037 },
-  { letter: "W", frequency: .02465 },
-  { letter: "X", frequency: .00150 },
-  { letter: "Y", frequency: .01971 },
-  { letter: "Z", frequency: .00074 }
-];
-
 // importing csv file with data into d3
 // d3.csv("file_name.csv")
-d3.csv("frequency_data.csv")
+d3.csv("sales_data.csv")
   // check if it was loaded - in browser's console (localhost:8000)
   .then(function (data) {
     console.log(data);
 
     // convert ot numbers
     data.forEach(function (d) {
-      d.frequency = Number(d.frequency); // or d.frequency = +d.frequency;
+      d.Sales = Number(d.Sales); // or d.Sales = +d.Sales;
     })
 
 
@@ -58,246 +27,177 @@ d3.csv("frequency_data.csv")
     var leftOffset = 50 + 0.5 * gap;
     var myWidth = (barWidth + gap) * dataCount;
 
-    /*
-        // declaree a scale for X axis
-        // mapping categorical values from the csv file
-        var xScale = d3.scaleBand()
-          .domain(data.map(function (d) {
-            return d.letter;
-          }))
-          .range([0, (myWidth + gap * (dataCount))]) // from 0 to (width of bar + gap)
-    
-    
-        // declare a scale for Y axis
-        var yScale = d3.scaleLinear()
-          // source of data - what is written on the axis
-          .domain([0, d3.max(data, function (d) {
-            return d.frequency;
-          }) + 0]) //.domain([0, 500])  // <-- a simple case
-          // mapped destination - where the axis is placed
-          .range([myHeight, 0]) // note the reveresed order for axes values going from bottom up
-          ; // <-- note semicolon here!
-    
-    
-        // create X axis
-        var xAxis = d3.axisBottom()
-          .scale(xScale);
-    
-        // create Y axis
-        var yAxis = d3.axisLeft()
-          .scale(yScale); // <-- semicolon!
-    
-    
-        // create a svg container (still inside the curly brackets!)
-        var svgContainer = d3.select("#dataViz2").append("svg")
-          .attr("height", myHeight + 75)
-          .attr("width", myWidth * dataCount)
-          .attr("transform", "translate(0, 10)"); // offset
-    
-    
-        // RECTANGLE BARS
-    
-        // generate rectangles for Bar Chart
-        // associate variable from imported file - .data(data) - with all rectangles,
-        var myRectangle = svgContainer.selectAll('rect').data(data);
-    
-    
-        // enter loops - create rectangle for each instance from the data
-        myRectangle.enter().append("rect")
-    
-          // transition state zero (initial)
-          .attr("fill", "#333")
-          // function will run and returns i (the index value of the array)
-          // d = data, i = index
-          .attr("x", function (d, i) {
-            return leftOffset + (i * (myWidth / dataCount + gap)); // initial offset (left margin) + width of rectangle (40) + 2 for gap between them
-          })
-          .attr("y", myHeight)
-          .attr("width", (myWidth / dataCount))
-    
-          // transition state one 
-          .transition()
-          .duration(1000)
-          //.attr("y", 50) // this would produce bars hanging down, from baseline 50px from top
-          .attr("y", function (d, i) {
-            return yScale(d.frequency) // baseline 300px from top, minus (i.e. going up) the height of the rect
-          })
-          // without scaling      //.attr("height", function (d) {return d.frequency;}) // takes value of frequency from the imported csv; *10 to magnify the result
-          // with scaling
-          .attr("height", function (d) {
-            return myHeight - yScale(d.frequency);
-          })
-          .attr("fill", "steelblue")
-    
-    
-        // LABELS 1 - frequency
-    
-        // add label text for Bar Chart, using values from imported file - .data(data)
-        var mylabel = svgContainer.selectAll('text').data(data);
-    
-        // enter loops through
-        mylabel.enter().append("text")
-    
-          // transition state zero (initial)
-          .attr("text-anchor", "middle")
-          .attr("font-family", "sans-serif")
-          .attr("font-size", "18px")
-          .attr("fill", "steelblue")
-          .attr("x", function (d, i) {
-            return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // width of rectangle (40) + 2 for gap between them
-          })
-          .attr("y", 350)
-    
-          // transition state one 
-          .transition()
-          .duration(2000)
-          .delay(function (d, i) {
-            return 650 + (i * 25);
-          })
-          .ease(d3.easeCubicInOut) // d3.easeCircleIn, d3.easeElasticOut, d3.easeLinear
-          // positioning
-          .attr("y", function (d) {
-            return (d3.max(data, function (d) { return d.frequency; }) * (myHeight / d3.max(data, function (d) { return d.frequency; })) + 20) - d.frequency * (myHeight / d3.max(data, function (d) { return d.frequency; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
-          })// max = 580, scale = 350
-          // text style
-          .attr("fill", "white")
-          // text content
-          .text(function (d) {
-            return d.frequency; // frequency from csv file + unicode degree symbol 
-          });
-    
-    
-        // LABELS 2 - letters
-    
-        // add label text for Bar Chart, using values from imported file - .data(data)
-        //var mylabel = svgContainer.selectAll('text').data(data);
-    
-        // enter loops through
-        mylabel.enter().append("text")
-    
-          // transition state zero (initial)
-          .transition()
-          .duration(0)
-          .delay(2500)
-          // positioning
-          .attr("x", function (d, i) {
-            return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // (50+20) to centre text in the middle of the bar; width of rectangle (40) + 2 for gap between them
-          })
-          .attr("y", 0.9 * myHeight)
-          // text style
-          .attr("text-anchor", "middle")
-          .attr("font-family", "sans-serif")
-          .attr("font-size", "8px")
-          .attr("fill", "steelblue")
-          // text content
-          .text(function (d) {
-            return d.letter; // letters names from the array
-          })
-    
-          // transition state two
-          .transition()
-          .duration(2500)
-          .delay(function (d, i) {
-            return 200 + (i * 250);
-          })
-          .ease(d3.easeBounceOut)
-          // Vertical positioning
-          .attr("y", function (d) {
-            return (d3.max(data, function (d) { return d.frequency; }) * (myHeight / d3.max(data, function (d) { return d.frequency; })) + 30) - d.frequency * (myHeight / d3.max(data, function (d) { return d.frequency; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
-          })
-          .attr("fill", "#ccc")
-    
-          // transition state three
-          .transition()
-          .duration(4000)
-          .delay(1000)
-          .attr("fill", "steelblue")
-          .ease(d3.easeLinear);
-    
-    
-        // AXES rendering
-    
-        svgContainer.append("g") // 'g' denotes group
-          .attr("transform", "translate(50," + myHeight + ")") // note that height is inside quote marks - it is a text that need to be concatinated!
-          .call(xAxis)
-          // apply the below to all text elements
-          .selectAll("text")
-          .attr("transform", "rotate(-60)")
-          .attr("text-anchor", "end")
-          .attr("x", "-8") // by trial and error
-          .attr("y", "8");
-    
-        svgContainer.append("g") // 'g' denotes group
-          .attr("transform", "translate(" + (leftOffset - 0.5 * gap) + ", 0)")
-          .call(yAxis);
-    */
+    // declaree a scale for X axis
+    // mapping categorical values from the csv file
+    var xScale = d3.scaleBand()
+      .domain(data.map(function (d) {
+        return d.Month;
+      }))
+      .range([0, (myWidth + gap * (dataCount))]) // from 0 to (width of bar + gap)
 
 
-    var margin = { top: 40, right: 20, bottom: 30, left: 40 },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+    // declare a scale for Y axis
+    var yScale = d3.scaleLinear()
+      // source of data - what is written on the axis
+      .domain([0, d3.max(data, function (d) {
+        return d.Sales;
+      }) + 0]) //.domain([0, 500])  // <-- a simple case
+      // mapped destination - where the axis is placed
+      .range([myHeight, 0]) // note the reveresed order for axes values going from bottom up
+      ; // <-- note semicolon here!
 
-    var formatPercent = d3.format(".0%");
 
-    var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
+    // create X axis
+    var xAxis = d3.axisBottom()
+      .scale(xScale);
 
-    var y = d3.scale.linear()
-      .range([height, 0]);
+    // create Y axis
+    var yAxis = d3.axisLeft()
+      .scale(yScale); // <-- semicolon!
 
-    var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
 
-    var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left")
-      .tickFormat(formatPercent);
+    // create a svg container (still inside the curly brackets!)
+    var svgContainer = d3.select("#dataVis2").append("svg")
+      .attr("height", myHeight + 75)
+      .attr("width", myWidth + 200)
+      .attr("transform", "translate(0, 10)"); // offset
 
-    var tip = d3.tip()
-      .attr('class', 'd3-tip')
-      .offset([-10, 0])
-      .html(function (d) {
-        return "<strong>Frequency:</strong> <span style='color:red'>" + d.frequency + "</span>";
+
+    // RECTANGLE BARS
+
+    // generate rectangles for Bar Chart
+    // associate variable from imported file - .data(data) - with all rectangles,
+    var myRectangle = svgContainer.selectAll('rect').data(data);
+
+
+    // enter loops - create rectangle for each instance from the data
+    myRectangle.enter().append("rect")
+
+      // transition state zero (initial)
+      .attr("fill", "#333")
+      // function will run and returns i (the index value of the array)
+      // d = data, i = index
+      .attr("x", function (d, i) {
+        return leftOffset + (i * (myWidth / dataCount + gap)); // initial offset (left margin) + width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", myHeight)
+      .attr("width", (myWidth / dataCount))
+
+      // transition state one 
+      .transition()
+      .duration(1000)
+      //.attr("y", 50) // this would produce bars hanging down, from baseline 50px from top
+      .attr("y", function (d, i) {
+        return yScale(d.Sales) // baseline 300px from top, minus (i.e. going up) the height of the rect
+      })
+      // without scaling      //.attr("height", function (d) {return d.Sales;}) // takes value of Sales from the imported csv; *10 to magnify the result
+      // with scaling
+      .attr("height", function (d) {
+        return myHeight - yScale(d.Sales);
+      })
+      .attr("fill", "#3ad")
+
+
+    // LABELS 1 - sales
+
+    // add label text for Bar Chart, using values from imported file - .data(data)
+    var mylabel = svgContainer.selectAll('text').data(data);
+
+    // enter loops through
+    mylabel.enter().append("text")
+
+      // transition state zero (initial)
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "18px")
+      .attr("fill", "steelblue")
+      .attr("x", function (d, i) {
+        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", 350)
+
+      // transition state one 
+      .transition()
+      .duration(2000)
+      .delay(function (d, i) {
+        return 650 + (i * 25);
+      })
+      .ease(d3.easeCubicInOut) // d3.easeCircleIn, d3.easeElasticOut, d3.easeLinear
+      // positioning
+      .attr("y", function (d) {
+        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 20) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
+      })// max = 580, scale = 350
+      // text style
+      .attr("fill", "#fd7")
+      // text content
+      .text(function (d) {
+        return d.Sales; // Sales from csv file + unicode degree symbol 
+      });
+
+
+    // LABELS 2 - months
+
+    // add label text for Bar Chart, using values from imported file - .data(data)
+    //var mylabel = svgContainer.selectAll('text').data(data);
+
+    // enter loops through
+    mylabel.enter().append("text")
+
+      // transition state zero (initial)
+      .transition()
+      .duration(0)
+      .delay(2500)
+      // positioning
+      .attr("x", function (d, i) {
+        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // (50+20) to centre text in the middle of the bar; width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", 0.9 * myHeight)
+      // text style
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "8px")
+      .attr("fill", "#3ad")
+      // text content
+      .text(function (d) {
+        return d.Month; // months names from csv file
       })
 
-    var svg = d3.select("body").append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      // transition state two
+      .transition()
+      .duration(2500)
+      .delay(function (d, i) {
+        return 200 + (i * 250);
+      })
+      .ease(d3.easeBounceOut)
+      // Vertical positioning
+      .attr("y", function (d) {
+        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 30) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
+      })
+      .attr("fill", "#fd7")
 
-    svg.call(tip);
-
-    d3.tsv("data.tsv", type, function (error, data) {
-      x.domain(data.map(function (d) { return d.letter; }));
-      y.domain([0, d3.max(data, function (d) { return d.frequency; })]);
-
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-      svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Frequency");
-
-      svg.selectAll(".bar")
-        .data(data)
-        .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function (d) { return x(d.letter); })
-        .attr("width", x.rangeBand())
-        .attr("y", function (d) { return y(d.frequency); })
-        .attr("height", function (d) { return height - y(d.frequency); })
-        .on('mouseover', tip.show)
-        .on('mouseout', tip.hide)
+      // transition state three
+      .transition()
+      .duration(4000)
+      .delay(1000)
+      .attr("fill", "#3ad")
+      .ease(d3.easeLinear);
 
 
-    });
+    // AXES rendering
+
+    svgContainer.append("g") // 'g' denotes group
+      .attr("transform", "translate(50," + myHeight + ")") // note that height is inside quote marks - it is a text that need to be concatinated!
+      .call(xAxis)
+      // apply the below to all text elements
+      .selectAll("text")
+      .attr("transform", "rotate(-60)")
+      .attr("text-anchor", "end")
+      .attr("x", "-8") // by trial and error
+      .attr("y", "8");
+
+    svgContainer.append("g") // 'g' denotes group
+      .attr("transform", "translate(" + (leftOffset - 0.5 * gap) + ", 0)")
+      .call(yAxis);
+
+
+  });
