2d83f679c7df257266661835d05db65bc8f27f9a
andkoc001 <and.koc001@gmail.com>
2020-05-12 22:47:50 +0200

    DOM page added

--- Project/wad_project.js
+++ Project/wad_project.js
@@ -243,3 +243,81 @@ d3.csv("sales_data.csv")
   });
 
 // -----------------
+// DOM manipulation
+
+// Style change
+function changeColours() {
+  document.getElementById("domStyle1").style.backgroundColor = "#124"
+  document.getElementById("domStyle1").style.color = "#fd7"
+}
+
+function changeTextStyle() {
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.fontStyle = "italic";
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.textTransform = "uppercase";
+}
+
+function resetStyle() {
+  document.getElementById("domStyle1").style.backgroundColor = "#234"
+  document.getElementById("domStyle1").style.color = "#eee"
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.fontStyle = "normal"
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.textTransform = "none";
+}
+
+// Content change
+function changeText() {
+  document.getElementById("sampleText").innerHTML = "This is DOM manipulation"
+}
+
+function addImage() {
+  document.getElementById("picture").src = "Harreck_Blue_Car.svg";
+  document.getElementById("picture").style.height = "100px";
+}
+
+
+function resetContent() {
+  document.getElementById("sampleText").innerHTML = "Sample text."
+  document.getElementById("picture").src = "";
+  document.getElementById("picture").style.height = "0";
+}
+
+// animation
+function ping() {
+  x = document.getElementById("pingpong")
+  x.style.animationTimingFunction = "ease-out";
+  x.style.animation = "mymove1 1s 1";
+  x.style.left = "830px";
+}
+
+function pong() {
+  x = document.getElementById("pingpong")
+  x.style.animationTimingFunction = "ease-out";
+  x.style.animation = "mymove2 1s 1";
+  x.style.left = "0px";
+}
+
+
+// User interface - Calculator
+
+function read() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  document.getElementById("readings").innerHTML = "Number 1: " + num1 + "</br>" + "Number 2: " + num2;
+  return num1, num2;
+}
+
+function add() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  document.getElementById("result").innerHTML = "ANSWER: " + num1 + " plus " + num2 + " = " + (num1 + num2);
+}
+
+function div() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  if (num2 != "0") {
+    document.getElementById("result").innerHTML = "ANSWER: " + num1 + " divided by " + num2 + " = " + (num1 / num2);
+  }
+  else {
+    document.getElementById("result").innerHTML = "Zero division error"
+  }
+}
