3490c667452dbff4f191c0042992fec4678e3533
andkoc001 <and.koc001@gmail.com>
2020-05-16 01:09:41 +0200

    Data visualisation from array improved

--- Project/datavis1.js
+++ Project/datavis1.js
@@ -42,96 +42,288 @@ var alphabet = [
 // plantes distance, diameter, density, gravity, moons relative to Earth (Earth = 1)
 // data extracted from: https://nssdc.gsfc.nasa.gov/planetary/factsheet/planet_table_ratio.html
 var planets = [
-  { planet: "Mercury", distance: "0.387", diameter: "0.383", density: ".984", gravity: "0.", moons: "0" },
+  { planet: "Mercury", distance: "0.387", diameter: "0.383", density: "0.984", gravity: "0.378", moons: "0" },
   { planet: "Venus", distance: "0.723", diameter: "0.949", density: "0.951", gravity: "0.907", moons: "0" },
   { planet: "Earth", distance: "1", diameter: "1", density: "1", gravity: "1", moons: "1" },
   { planet: "Mars", distance: "1.52", diameter: "0.532", density: "0.713", gravity: "0.377", moons: "2" },
-  { planet: "Jupyter", distance: "5.2", diameter: "11.21", density: "0.24", gravity: "2.36", moons: "79" },
+  { planet: "Jupyter", distance: "5.2", diameter: "11.21", density: "0.240", gravity: "2.36", moons: "79" },
   { planet: "Saturn", distance: "9.58", diameter: "9.45", density: "0.125", gravity: "0.916", moons: "82" },
-  { planet: "Uranus", distance: "19.2", diameter: "4.01", density: "0.23", gravity: "0.889", moons: "27" },
-  { planet: "Neptune", distance: "30.05", diameter: "3.88", density: ".297", gravity: "1.12", moons: "14" }
+  { planet: "Uranus", distance: "19.2", diameter: "4.01", density: "0.230", gravity: "0.889", moons: "27" },
+  { planet: "Neptune", distance: "30.05", diameter: "3.88", density: "0.297", gravity: "1.12", moons: "14" }
 ];
 
 
-// create svg container (canvas)
+
+// -------------------
+// draw chart function - diameter
+
 var svgContainer = d3.select("#dataVis1").append("svg")
   .attr("width", 400)
-  .attr("height", 2000);
+  .attr("height", 200);
 
 
-// draw Sun 
-var sun = svgContainer.append("ellipse")
+function drawChartDia2() {
+  eraseChart2()
 
-  // transition state zero (initial)
-  .attr("rx", 1)
-  .attr("ry", 1)
-  .attr("cx", 200)
-  .attr("cy", -1040)
-  .attr("fill", "#fd7")
-  .attr("stroke", "#f80")
-  .attr("stroke-width", 2)
-
-  // transition state one 
-  .transition()
-  .attr("rx", 1090)
-  .attr("ry", 1090)
-  .delay(1000)
-  .duration(2000);
+  // create svg container (canvas)
+  var svgContainer = d3.select("#dataVis1").append("svg")
+    .attr("width", 400)
+    .attr("height", 2000);
 
 
-// draw plantes diameter
-var circles = svgContainer.selectAll("circle")
-  .data(planets);
+  // draw Sun 
+  var sun = svgContainer.append("ellipse")
 
-circles.enter().append("circle")
+    // transition state zero (initial)
+    .attr("rx", 1)
+    .attr("ry", 1)
+    .attr("cx", 200)
+    .attr("cy", -1040)
+    .attr("fill", "#fd7")
+    .attr("stroke", "#f80")
+    .attr("stroke-width", 2)
 
-  // transition state zero (initial)
-  .attr("cx", 200)
-  .attr("cy", -200)
-  .attr("r", function (d) {
-    return d.diameter * 1;
-  })
-  .attr("fill", function (d, i) { return "#" + (1 + i) + "" + (1 + i) + "f" }) // each iteration changes the red and green element of the #RGB parameter
-
-  // transition state one 
-  .transition()
-  .duration(1500) // in miliseconds
-  // move the plantes down to their locations
-  .attr("cy", function (d, i) { return 150 + (i * 250); })
-  // for better impression, the size is magnified 10-folds
-  .attr("r", function (d) {
-    return d.diameter * 10;
-  })
-  // the transition on each iteration has incrementally different duration
-  .delay(function (d, i) {
-    return 1200 - (i * 150);
-  })
-  .duration(2000);
+    // transition state one 
+    .transition()
+    .attr("rx", 1090)
+    .attr("ry", 1090)
+    .delay(1000)
+    .duration(2000);
 
 
+  // draw plantes diameter
+  var circles = svgContainer.selectAll("circle")
+    .data(planets);
 
-// draw labels
-var circles = svgContainer.selectAll("text")
-  .data(planets);
+  circles.enter().append("circle")
 
-circles.enter().append("text")
-  .text("Sun")
-  .attr("x", 186)  // centre the word
-  .attr("y", -30) // initially out of the view
-  .style("fill", "#334")
-  .transition()
-  .delay(2650)
-  .ease(d3.easeCircleOut)
-  .attr("y", 30);
+    // transition state zero (initial)
+    .attr("cx", 200)
+    .attr("cy", -200)
+    .attr("r", function (d) {
+      return d.diameter * 1;
+    })
+    .attr("fill", function (d, i) { return "#" + (1 + i) + "" + (1 + i) + "f" }) // each iteration changes the red and green element of the #RGB parameter
+
+    // transition state one 
+    .transition()
+    .duration(1500) // in miliseconds
+    // move the plantes down to their locations
+    .attr("cy", function (d, i) { return 150 + (i * 250); })
+    // for better impression, the size is magnified 10-folds
+    .attr("r", function (d) {
+      return d.diameter * 10;
+    })
+    // the transition on each iteration has incrementally different duration
+    .delay(function (d, i) {
+      return 750 - (i * 150);
+    })
+    .duration(2000)
+    .ease(d3.easeBack);
 
 
-circles.enter().append("text")
-  .attr("x", 20)
-  .attr("y", function (d, i) { return 155 + (i * 250); })
-  .text(function (d, i) { return d.planet });
+
+  // draw labels
+  var circles = svgContainer.selectAll("text")
+    .data(planets);
+
+  circles.enter().append("text")
+    .text("Sun")
+    .attr("x", 186)  // centre the word
+    .attr("y", -30) // initially out of the view
+    .style("fill", "#334")
+    .transition()
+    .delay(2650)
+    .ease(d3.easeCircleOut)
+    .attr("y", 30);
 
 
-circles.enter().append("text")
-  .attr("x", 320)
-  .attr("y", function (d, i) { return 155 + (i * 250); })
-  .text(function (d, i) { return "R = " + d.diameter });
+  circles.enter().append("text")
+    .attr("x", -100)
+    .attr("y", function (d, i) { return 155 + (i * 250); })
+    .text(function (d, i) { return d.planet })
+    .transition()
+    .attr("x", 20)
+    .delay(500)
+    .duration(1500);
+
+
+  circles.enter().append("text")
+    .attr("x", 420)
+    .attr("y", function (d, i) { return 155 + (i * 250); })
+    .text(function (d, i) { return "R = " + d.diameter })
+    .transition()
+    .attr("x", 320)
+    .delay(500)
+    .duration(1500);
+
+} // end of drawChart function
+
+
+// -------------------
+// draw chart function - density
+
+function drawChartDens2() {
+  eraseChart2()
+
+  // create svg container (canvas)
+  var svgContainer = d3.select("#dataVis1").append("svg")
+    .attr("width", 400)
+    .attr("height", 2000);
+
+  /*
+    // draw Sun 
+    var sun = svgContainer.append("ellipse")
+  
+      // transition state zero (initial)
+      .attr("rx", 1)
+      .attr("ry", 1)
+      .attr("cx", 200)
+      .attr("cy", -1040)
+      .attr("fill", "#fd7")
+      .attr("stroke", "#f80")
+      .attr("stroke-width", 2)
+  
+      // transition state one 
+      .transition()
+      .attr("rx", 1090)
+      .attr("ry", 1090)
+      .delay(1000)
+      .duration(2000);
+  */
+
+  // draw plantes density
+  var circles = svgContainer.selectAll("circle")
+    .data(planets);
+
+  circles.enter().append("circle")
+
+    // transition state zero (initial)
+    .attr("cx", 200)
+    .attr("cy", function (d, i) { return 140 + (i * 250); })
+    .attr("r", 0)
+    // the fill colour depends on the density - the value from the array is converted to a hexadecimal value 
+    .attr("fill", "#fff")
+    .style("stroke-width", 0)
+
+    // transition state one 
+    .transition()
+    .duration(4000) // in miliseconds
+    // for better impression, the size is magnified 50-folds
+    .attr("r", function (d) {
+      return d.density * 100;
+    })
+
+    // the transition on each iteration has incrementally different duration
+    .attr("fill", function (d, i) {
+      return "#" +
+        (Math.floor((Number((1 - d.density) * 15))).toString(16)) +
+        (Math.floor((Number((1 - d.density) * 15))).toString(16)) +
+        (Math.floor((Number((1 - d.density) * 15))).toString(16))
+    });
+
+
+  // Earths reference countur
+  circles.enter().append("circle")
+    .attr("cx", 200)
+    .attr("cy", function (d, i) { return 140 + (i * 250); })
+    .attr("r", 100)
+    .style("fill", "none")
+    .style("stroke", "#234")
+
+    .transition()
+    .delay(1000)
+    .duration(2000)
+    .style("stroke", "#fd7")
+    .style("stroke-dasharray", ("3, 3"))
+    .style("stroke-width", 1);
+
+  circles.enter().append("line")
+    .attr("x1", 175)
+    .attr("y1", 20)
+    .attr("x2", 225)
+    .attr("y2", 20)
+
+    .transition()
+    .style("fill", "#234")
+    .style("stroke-dasharray", ("3, 3"))
+    .style("stroke-width", .1)
+
+    .delay(2000)
+    .duration(2000)
+    .style("fill", "#fd7");
+
+  var circles = svgContainer.selectAll("text")
+    .data(planets);
+
+  circles.enter().append("text")
+    .attr("x", 250)  // centre the word
+    .attr("y", 24) // initially out of the view
+    .text("\u27F5 Earth reference")
+    .style("fill", "#234")
+
+    .transition()
+    .delay(2000)
+    .duration(2000)
+    .style("fill", "#eee")
+
+
+  // draw labels
+  circles.enter().append("text")
+    .attr("x", -100)
+    .attr("y", function (d, i) { return 145 + (i * 250); })
+    .text(function (d, i) { return d.planet })
+
+    .transition()
+    .attr("x", 20)
+    .delay(500)
+    .duration(1500);
+
+
+  circles.enter().append("text")
+    .attr("x", 420)
+    .attr("y", function (d, i) { return 145 + (i * 250); })
+    .text(function (d, i) { return "d = " + d.density })
+    .transition()
+    .attr("x", 320)
+    .delay(500)
+    .duration(1500);
+
+} // end of drawChart function
+
+
+// erase active chart function
+function eraseChart2() {
+
+  // create svg container (canvas)
+  var svgContainer = d3.selectAll("svg").remove();
+
+  var svgContainer = d3.select("#dataVis1").append("svg")
+    .attr("width", 400)
+    .attr("height", 0);
+
+}
+
+
+
+// -------------------
+// dom manipulation
+
+// Content change
+function drawChartDia() {
+  document.getElementById("planetsH2").innerHTML = "Planets Diameter"
+  document.getElementById("planetsP").innerHTML = "Below are visualised relative diameters (Earth = 1) of the planets in the Solar System. The planets are shown from top to bottom in order from neares to the Sun (Mercury) to the furthest (Neptune). The distance in this visualiation is neglected and, for simplicity, kept the same."
+  drawChartDia2()
+}
+
+function drawChartDens() {
+  document.getElementById("planetsH2").innerHTML = "Planets Density"
+  document.getElementById("planetsP").innerHTML = "Below are visualised relative density of the planets in the Solar System (Earth = 1). The planets are shown from top to bottom in order from neares to the Sun (Mercury) to the furthest (Neptune). The distance in this visualiation is neglected and, for simplicity, kept the same. The colour (shade of gray) represents the density - darker means densier. There is also Earth's density countur (being the densierst planet) shown in dashed blue line."
+  drawChartDens2()
+}
+
+function eraseChart() {
+  document.getElementById("planetsH2").innerHTML = ""
+  document.getElementById("planetsP").innerHTML = ""
+  eraseChart2()
+}
--- Project/wad_project.js
+++ Project/wad_project.js
@@ -7,6 +7,322 @@ Lecturer: dr. Michael Duignan
 
 
 // -----------------
+// html form validation 
+
+// Form submit button
+function submitAll() {
+  document.getElementById("newCarForm").submit();
+}
+
+// Form reset button
+function resetAll() {
+  document.getElementById("newCarForm").reset();
+}
+
+
+// ////////////////////
+
+// DOM manipulation
+
+// While filling out form fields with clas .highlight1, text color changes
+
+function myFunctionIn() {
+  var x, i;
+  x = document.querySelectorAll(".highlight1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.color = "blue";
+  }
+}
+
+function myFunctionOut() {
+  var x, i;
+  x = document.querySelectorAll(".highlight1");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.color = "black";
+  }
+}
+
+
+// ////////////////////
+
+// D3 
+
+// importing csv file with data into d3
+// d3.csv("file_name.csv")
+d3.csv("sales_data.csv")
+  // check if it was loaded - in browser's console (localhost:8000)
+  .then(function (data) {
+    console.log(data);
+
+    // convert ot numbers
+    data.forEach(function (d) {
+      d.Sales = Number(d.Sales); // or d.Sales = +d.Sales;
+    })
+
+
+    // Variables for chart bar values
+    var myHeight = 350;
+    var barWidth = 40;
+    var dataCount = data.length;
+    var gap = 8;
+    var leftOffset = 50 + 0.5 * gap;
+    var myWidth = (barWidth + gap) * dataCount;
+
+    // declaree a scale for X axis
+    // mapping categorical values from the csv file
+    var xScale = d3.scaleBand()
+      .domain(data.map(function (d) {
+        return d.Month;
+      }))
+      .range([0, (myWidth + gap * (dataCount))]) // from 0 to (width of bar + gap)
+
+
+    // declare a scale for Y axis
+    var yScale = d3.scaleLinear()
+      // source of data - what is written on the axis
+      .domain([0, d3.max(data, function (d) {
+        return d.Sales;
+      }) + 0]) //.domain([0, 500])  // <-- a simple case
+      // mapped destination - where the axis is placed
+      .range([myHeight, 0]) // note the reveresed order for axes values going from bottom up
+      ; // <-- note semicolon here!
+
+
+    // create X axis
+    var xAxis = d3.axisBottom()
+      .scale(xScale);
+
+    // create Y axis
+    var yAxis = d3.axisLeft()
+      .scale(yScale); // <-- semicolon!
+
+
+    // create a svg container (still inside the curly brackets!)
+    var svgContainer = d3.select("#myDiv").append("svg")
+      .attr("height", myHeight + 75)
+      .attr("width", myWidth * dataCount)
+      .attr("transform", "translate(0, 10)"); // offset
+
+
+    // RECTANGLE BARS
+
+    // generate rectangles for Bar Chart
+    // associate variable from imported file - .data(data) - with all rectangles,
+    var myRectangle = svgContainer.selectAll('rect').data(data);
+
+
+    // enter loops - create rectangle for each instance from the data
+    myRectangle.enter().append("rect")
+
+      // transition state zero (initial)
+      .attr("fill", "#333")
+      // function will run and returns i (the index value of the array)
+      // d = data, i = index
+      .attr("x", function (d, i) {
+        return leftOffset + (i * (myWidth / dataCount + gap)); // initial offset (left margin) + width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", myHeight)
+      .attr("width", (myWidth / dataCount))
+
+      // transition state one 
+      .transition()
+      .duration(1000)
+      //.attr("y", 50) // this would produce bars hanging down, from baseline 50px from top
+      .attr("y", function (d, i) {
+        return yScale(d.Sales) // baseline 300px from top, minus (i.e. going up) the height of the rect
+      })
+      // without scaling      //.attr("height", function (d) {return d.Sales;}) // takes value of Sales from the imported csv; *10 to magnify the result
+      // with scaling
+      .attr("height", function (d) {
+        return myHeight - yScale(d.Sales);
+      })
+      .attr("fill", "steelblue")
+
+
+    // LABELS 1 - sales
+
+    // add label text for Bar Chart, using values from imported file - .data(data)
+    var mylabel = svgContainer.selectAll('text').data(data);
+
+    // enter loops through
+    mylabel.enter().append("text")
+
+      // transition state zero (initial)
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "18px")
+      .attr("fill", "steelblue")
+      .attr("x", function (d, i) {
+        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", 350)
+
+      // transition state one 
+      .transition()
+      .duration(2000)
+      .delay(function (d, i) {
+        return 650 + (i * 25);
+      })
+      .ease(d3.easeCubicInOut) // d3.easeCircleIn, d3.easeElasticOut, d3.easeLinear
+      // positioning
+      .attr("y", function (d) {
+        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 20) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
+      })// max = 580, scale = 350
+      // text style
+      .attr("fill", "white")
+      // text content
+      .text(function (d) {
+        return d.Sales; // Sales from csv file + unicode degree symbol 
+      });
+
+
+    // LABELS 2 - months
+
+    // add label text for Bar Chart, using values from imported file - .data(data)
+    //var mylabel = svgContainer.selectAll('text').data(data);
+
+    // enter loops through
+    mylabel.enter().append("text")
+
+      // transition state zero (initial)
+      .transition()
+      .duration(0)
+      .delay(2500)
+      // positioning
+      .attr("x", function (d, i) {
+        return (leftOffset + 0.5 * myWidth / dataCount) + (i * (myWidth / dataCount + gap)); // (50+20) to centre text in the middle of the bar; width of rectangle (40) + 2 for gap between them
+      })
+      .attr("y", 0.9 * myHeight)
+      // text style
+      .attr("text-anchor", "middle")
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "8px")
+      .attr("fill", "steelblue")
+      // text content
+      .text(function (d) {
+        return d.Month; // months names from csv file
+      })
+
+      // transition state two
+      .transition()
+      .duration(2500)
+      .delay(function (d, i) {
+        return 200 + (i * 250);
+      })
+      .ease(d3.easeBounceOut)
+      // Vertical positioning
+      .attr("y", function (d) {
+        return (d3.max(data, function (d) { return d.Sales; }) * (myHeight / d3.max(data, function (d) { return d.Sales; })) + 30) - d.Sales * (myHeight / d3.max(data, function (d) { return d.Sales; }))  // baseline from top + 20px into the bar, minus (i.e. going up) the height of the rect
+      })
+      .attr("fill", "#ccc")
+
+      // transition state three
+      .transition()
+      .duration(4000)
+      .delay(1000)
+      .attr("fill", "steelblue")
+      .ease(d3.easeLinear);
+
+
+    // AXES rendering
+
+    svgContainer.append("g") // 'g' denotes group
+      .attr("transform", "translate(50," + myHeight + ")") // note that height is inside quote marks - it is a text that need to be concatinated!
+      .call(xAxis)
+      // apply the below to all text elements
+      .selectAll("text")
+      .attr("transform", "rotate(-60)")
+      .attr("text-anchor", "end")
+      .attr("x", "-8") // by trial and error
+      .attr("y", "8");
+
+    svgContainer.append("g") // 'g' denotes group
+      .attr("transform", "translate(" + (leftOffset - 0.5 * gap) + ", 0)")
+      .call(yAxis);
+
+  });
+
+// -----------------
+// DOM manipulation
+
+// Style change
+function changeColours() {
+  document.getElementById("domStyle1").style.backgroundColor = "#124"
+  document.getElementById("domStyle1").style.color = "#fd7"
+}
+
+function changeTextStyle() {
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.fontStyle = "italic";
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.textTransform = "uppercase";
+}
+
+function resetStyle() {
+  document.getElementById("domStyle1").style.backgroundColor = "#234"
+  document.getElementById("domStyle1").style.color = "#eee"
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.fontStyle = "normal"
+  document.getElementById("domStyle1").getElementsByTagName('p')[0].style.textTransform = "none";
+}
+
+// Content change
+function changeText() {
+  document.getElementById("sampleText").innerHTML = "This is DOM manipulation"
+}
+
+function addImage() {
+  document.getElementById("picture").src = "Harreck_Blue_Car.svg";
+  document.getElementById("picture").style.height = "100px";
+}
+
+
+function resetContent() {
+  document.getElementById("sampleText").innerHTML = "Sample text."
+  document.getElementById("picture").src = "";
+  document.getElementById("picture").style.height = "0";
+}
+
+// animation
+function ping() {
+  x = document.getElementById("pingpong")
+  x.style.animationTimingFunction = "ease-out";
+  x.style.animation = "mymove1 1s 1";
+  x.style.left = "830px";
+}
+
+function pong() {
+  x = document.getElementById("pingpong")
+  x.style.animationTimingFunction = "ease-out";
+  x.style.animation = "mymove2 1s 1";
+  x.style.left = "0px";
+}
+
+
+// User interface - Calculator
+
+function read() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  document.getElementById("readings").innerHTML = "Number 1: " + num1 + "</br>" + "Number 2: " + num2;
+  return num1, num2;
+}
+
+function add() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  document.getElementById("result").innerHTML = "ANSWER: " + num1 + " plus " + num2 + " = " + (num1 + num2);
+}
+
+function div() {
+  var num1 = Number(document.getElementById("box1").value);
+  var num2 = Number(document.getElementById("box2").value);
+  if (num2 != "0") {
+    document.getElementById("result").innerHTML = "ANSWER: " + num1 + " divided by " + num2 + " = " + (num1 / num2);
+  }
+  else {
+    document.getElementById("result").innerHTML = "Zero division error"
+  }
+}
+
+// -----------------
 // Go to top button; adopted from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 // Get the button
 var mybutton = document.getElementById("toTop");
